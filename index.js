ec5c24bea7c01265b73fbae07bf06c0c694b71e7
Your Name <matthmella@gmail.com>
2024-04-18 13:34:21 -0600

    suh dude

--- index.js
+++ index.js
@@ -13,12 +13,20 @@ const app = express();
 // Port number can be set in the environment or default to 4000
 const PORT = process.env.PORT || 4000;
 
-const corsOptions = {
-  origin: '*',  // This allows all domains; change to your frontend domain in production
-  methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
-};
+const allowedOrigins = ['https://startup.cs260party.click'];
 
-app.use(cors(corsOptions));
+app.use(cors({
+    origin: function (origin, callback) {
+        // Allow requests with no origin like mobile apps or curl requests
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
+    credentials: true
+}));
 
 
 // Serve static files from the 'public' directory
